dcf3907ad1ade6be95db1d9fcaa0aa1497ea8c1f
BobbyLin <linzhangsheng23@gmail.com>
2025-08-11 22:45:42 +0800

    chore: add db

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import process from 'node:process'
+import { config } from 'dotenv'
+import { defineConfig } from 'drizzle-kit'
+
+config({ path: '.env' })
+
+export default defineConfig({
+  schema: './server/db/schema.ts',
+  out: './server/db/migrations',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+})
